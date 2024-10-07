dbc812a9788deb7771b682140ca2883c89e0c76b
tesar-tech <jan@tesar.tech>
2024-10-07 09:27:31 +0200

    Template to version 1.0.2

--- BlazorStaticTemplates/pack_and_test.ps1
+++ BlazorStaticTemplates/pack_and_test.ps1
@@ -7,7 +7,7 @@ Write-Host "Uninstalling the old template 'BlazorStatic.Templates'..." -Foregrou
 dotnet new uninstall BlazorStatic.Templates
 
 Write-Host "Installing the new template" -ForegroundColor Cyan
-dotnet new install --force "bin/Release/BlazorStatic.Templates.1.0.0.nupkg"
+dotnet new install --force "bin/Release/BlazorStatic.Templates.1.0.2.nupkg"
 
 Write-Host "Removing the old 'TestProject' directory..." -ForegroundColor Cyan
 Remove-Item -Recurse -Force -Path ".\TestProject"
